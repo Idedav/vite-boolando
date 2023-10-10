fe75ca9c9a5b07de480c8f570b1a57000e78b93f
Idedav <xdavide.correa@gmail.com>
2023-10-10 14:15:17 +0200

    [add] components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
   export default {
-    
+    name: 'App',
+    components:{
+      Header,
+      Main,
+      Footer
+    }
   }
 </script>
 
 
 <template>
-  
+  <Header />
+  <Main />
+  <Footer />
 </template>
 
 
